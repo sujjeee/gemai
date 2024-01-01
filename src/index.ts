4383ad3711a3656c84dbb6062bb565bf634f7e79
Suraj <sujjeee@outlook.com>
2024-01-02 00:47:01 +0530

    updated index

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,25 @@
 import { chat } from "@/commands/chat";
 import { config } from "@/commands/config";
 import { login } from "@/commands/login";
-
+import { getPackageInfo } from "@/utils/get-package-info";
 import { Command } from "commander";
 
 process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
 
-function main() {
+async function main() {
+  const packageInfo = await getPackageInfo();
+
   const program = new Command()
     .name("gemai")
     .description("AI chatbot in a terminal environment.")
-    .version("1.0.0", "-v, --version", "display the version number");
+    .version(
+      packageInfo.version || "1.0.0",
+      "-v, --version",
+      "display the version number"
+    );
 
-  program.addCommand(login).addCommand(config);
-  program.addCommand(chat);
+  program.addCommand(login).addCommand(config).addCommand(chat);
   program.parse();
 }
 
