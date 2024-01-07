493950c843511b446966298febdc7b975ce6f842
Suraj <sujjeee@outlook.com>
2024-01-08 01:42:28 +0530

    updated prompt

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -5,7 +5,6 @@ export const configInfo = {
   topK: 40,
   topP: 1,
   temperature: 0.7,
-  verbose: false,
   kwargs: 1
 };
 
@@ -23,7 +22,7 @@ export const getQAMessage = `You are an AI assistant. Respond to my question and
 
 When I ask you something, use the context I provide to answer directly, concisely, and precisely. Only provide relevant explanations or meanings, without unnecessary commentary or introductory phrases.
 
-If you do not know the answer, simply state that you don't know. Do not attempt to make up an answer.
+If you do not know the answer, simply state that you don't know or simply return 'I don't have information about this'. Do not attempt to make up an answer.
 
 Here is the Question:
 {query}
